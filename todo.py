9da08789b6ec7d3c019915136488a7351bcf362f
cgrkzlkn <cgrkzlkn@gmail.com>
2020-11-01 15:31:32 +0300

    SQLAlchemy path updated

--- todo.py
+++ todo.py
@@ -1,8 +1,10 @@
 from flask import Flask, render_template,request,redirect,url_for
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////Users/90537/Desktop/TodoApp/todo.db'
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'todo.db')
 db = SQLAlchemy(app)
 
 @app.route("/")
